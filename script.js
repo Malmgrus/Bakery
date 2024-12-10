3a1d307e825ada0f535243dd6415048e0d1a6be0
Malmgrus <malmros.mattias@gmail.com>
2024-12-10 14:27:55 +0100

    began work on shop.html and animation for rubric

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function show(shown, hidden) {
+    document.getElementById(shown).style.display='block';
+    document.getElementById(hidden).style.display='none';
+    return false;
+}
 
+function animationRubric() {
+    anime({
+        targets: '#headline path',
+        strokeDashoffset: [anime.setDashoffset, 0],
+        easing: 'easeInOutSine',
+        duration: 1500,
+        delay: function(el, i) { return i * 250 },
+        direction: 'alternate',
+        loop: true
+    });
+}
